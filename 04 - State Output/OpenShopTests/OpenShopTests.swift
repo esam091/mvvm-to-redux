d637e8fdffa700af0064b307f2f99e6572e2ec42
Samuel Edwin <esam091@gmail.com>
2020-04-21 09:12:05 +0700

    Fix the rest of the tests

--- 04 - State Output/OpenShopTests/OpenShopTests.swift
+++ 04 - State Output/OpenShopTests/OpenShopTests.swift
@@ -77,98 +77,99 @@ class OpenShopTests: XCTestCase {
         input.onNext(.shopNameDidChange("something"))
 
         state.assertLastValue(State(shopName: "something", selectedDomainName: "foo"))
-        
-
     }
 
-//    func test_inputShopName_showDomainSuggestionWithErrors() {
-//        let called = expectation(description: "use case called")
-//
-//        useCase.checkShopName = { _ in
-//            Driver.just(ValidateShopNameResponse(suggestedDomain: "bar", shopNameErrorMessage: "error message"))
-//                .delay(.nanoseconds(1))
-//                .do(onNext: { _ in called.fulfill() })
-//        }
-//
-//        input.onNext(.shopNameDidChange("another thing"))
-//
-//        wait(for: [called], timeout: 0.001)
-//
-//        shopNameError.assertValues([nil, "error message"])
-//        domainName.assertValues(["bar"])
-//    }
-//
-//    func test_inputDomainName_valid() {
-//        useCase.checkDomainName = { _ in
-//            .just(.success(()))
-//        }
-//
-//        input.onNext(.shopDomainDidChange("foo"))
-//
-//        domainNameError.assertValues([nil, nil])
-//    }
-//
-//    func test_inputDomainName_invalid() {
-//        useCase.checkDomainName = { _ in
-//            .just(.failure(SimpleErrorMessage(message: "error")))
-//        }
-//
-//        input.onNext(.shopDomainDidChange("foo"))
-//
-//        domainNameError.assertValues([nil, "error"])
-//    }
-//
-//    func test_inputCity_dismissed() {
-//        input.onNext(.cityDidDismissed)
-//
-//        citySelectionError.assertValues([.dismissed])
-//    }
-//
-//    func test_inputCity_success() {
-//        let city = City(id: 1, name: "Hyrule")
-//        input.onNext(.cityDidSelected(city))
-//
-//        selectedCity.assertValues([city])
-//
-//            input.onNext(.cityDidDismissed)
-//
-//        selectedCity.assertValues([city])
-//    }
-//
-//    func test_inputDistrict_success() {
-//        let city = City(id: 2, name: "Tokyo")
-//        let district = District(id: 1, name: "Shibuya")
-//
-//        input.onNext(.cityDidSelected(city))
-//
-//        input.onNext(.districtDidTapped)
-//
-//        showDistrictSelection.assertDidEmitValues(count: 1)
-//
-//        input.onNext(.districtDidSelected(district))
-//
-//        selectedDistrict.assertValues([district])
-//    }
-//
-//    func test_inputDistrict_noCitySelected() {
-//        input.onNext(.districtDidTapped)
-//
-//        districtSelectionError.assertValues([.noCitySelected])
-//    }
-//
-//    func test_inputDistrict_dismissed() {
-//        let city = City(id: 2, name: "Tokyo")
-//
-//        input.onNext(.cityDidSelected(city))
-//
-//        input.onNext(.districtDidTapped)
-//
-//        showDistrictSelection.assertDidEmitValues(count: 1)
-//
-//        input.onNext(.districtDidDismissed)
-//
-//        districtSelectionError.assertValues([nil, .dismissed])
-//    }
+    func test_inputShopName_showDomainSuggestionWithErrors() {
+        let called = expectation(description: "use case called")
+
+        useCase.checkShopName = { _ in
+            Driver.just(ValidateShopNameResponse(suggestedDomain: "bar", shopNameErrorMessage: "error message"))
+                .delay(.nanoseconds(1))
+                .do(onNext: { _ in called.fulfill() })
+        }
+
+        input.onNext(.shopNameDidChange("another thing"))
+        state.assertLastValue(State(shopName: "another thing"))
+
+        wait(for: [called], timeout: 0.001)
+
+        
+        state.assertLastValue(State(shopName: "another thing", shopNameErrorMessage: "error message", selectedDomainName: "bar"))
+    }
+
+    func test_inputDomainName_valid() {
+        useCase.checkDomainName = { _ in
+            .just(.success(()))
+        }
+
+        input.onNext(.shopDomainDidChange("foo"))
+
+        state.assertLastValue(State(selectedDomainName: "foo"))
+    }
+
+    func test_inputDomainName_invalid() {
+        useCase.checkDomainName = { _ in
+            .just(.failure(SimpleErrorMessage(message: "error")))
+        }
+
+        input.onNext(.shopDomainDidChange("foo"))
+
+        state.assertLastValue(State(selectedDomainName: "foo", domainErrorMessage: "error"))
+    }
+
+    func test_inputCity_dismissed() {
+        input.onNext(.cityDidDismissed)
+        
+        state.assertLastValue(State(cityError: .dismissed))
+    }
+
+    func test_inputCity_success() {
+        let city = City(id: 1, name: "Hyrule")
+        input.onNext(.cityDidSelected(city))
+
+        state.assertLastValue(State(city: city))
+
+        input.onNext(.cityDidDismissed)
+
+        state.assertLastValue(State(city: city))
+    }
+
+    func test_inputDistrict_success() {
+        let city = City(id: 2, name: "Tokyo")
+        let district = District(id: 1, name: "Shibuya")
+
+        input.onNext(.cityDidSelected(city))
+        state.assertLastValue(State(city: city))
+
+        input.onNext(.districtDidTapped)
+
+        showDistrictSelection.assertDidEmitValues(count: 1)
+
+        input.onNext(.districtDidSelected(district))
+
+        state.assertLastValue(State(city: city, district: district))
+    }
+
+    func test_inputDistrict_noCitySelected() {
+        input.onNext(.districtDidTapped)
+
+        state.assertLastValue(State(districtError: .noCitySelected))
+    }
+
+    func test_inputDistrict_dismissed() {
+        let city = City(id: 2, name: "Tokyo")
+
+        input.onNext(.cityDidSelected(city))
+        state.assertLastValue(State(city: city))
+
+        input.onNext(.districtDidTapped)
+
+        showDistrictSelection.assertDidEmitValues(count: 1)
+
+        input.onNext(.districtDidDismissed)
+
+        state.assertLastValue(State(city: city, districtError: .dismissed))
+    }
     
     func test_allFieldsValid_willSubmitToServer() {
         let city = City(id: 2, name: "Tokyo")
@@ -182,13 +183,17 @@ class OpenShopTests: XCTestCase {
         useCase.submit = { _ in
             .just(.success(()))
         }
+        
         input.onNext(.shopNameDidChange("foo-shop"))
+        state.assertLastValue(State(shopName: "foo-shop", selectedDomainName: "foo-domain"))
         
         input.onNext(.cityDidSelected(city))
+        state.assertLastValue(State(shopName: "foo-shop", selectedDomainName: "foo-domain", city: city))
         
         input.onNext(.districtDidTapped)
         
         input.onNext(.districtDidSelected(district))
+        state.assertLastValue(State(shopName: "foo-shop", selectedDomainName: "foo-domain", city: city, district: district))
         
         input.onNext(.submitButtonDidTap)
         
